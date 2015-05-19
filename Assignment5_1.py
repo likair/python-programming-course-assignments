7f60d3f6776a5f1a3bbfe062a94c309d2c2753fa
Lebs <lebs.fi@qq.com>
2015-05-19 22:09:52 +0300

    Assignment 5_1

--- Assignment5_1.py
+++ Assignment5_1.py
@@ -10,6 +10,7 @@ Created on 19.5.2015
     
 @author: e1201757
 '''
+import os
 FILEPATH = 'product_db.txt'
 
 def writeToFile(content, filePath):
@@ -18,40 +19,85 @@ def writeToFile(content, filePath):
     file.close()
 
 def readFromFile(filePath):
-    file = open(filePath, 'r')
-    content = file.read().split('\n')
-    content.pop()
-    productsList = []
-    for product in content:
-        productDict = {}
-        details = product.split(';')
-        productDict['name'] = details[0]
-        productDict['unit_price'] = details[1]
-        productDict['amount'] = details[2]
-        productsList.append(productDict)
-    return productsList
+    if os.path.exists(filePath):
+        file = open(filePath, 'r')
+        content = file.read().split('\n')
+        content.pop()
+        productsList = []
+        for product in content:
+            productDict = {}
+            details = product.split(';')
+            productDict['name'] = details[0]
+            productDict['unit_price'] = details[1]
+            productDict['amount'] = details[2]
+            productsList.append(productDict)
+        return productsList
+    return []
         
 def inputProductsFromConsole():
     text = ''
     while True:
-        text = input('Please enter product data (name, unit price and amount), and use \';\' to separate(enter \'q\' to quit):')
-        if text != 'q':
+        print('Please enter product data (name;unit price;amount), and use \';\' to separate(enter \'q\' to quit).')
+        print('For example: Apple;2;100')
+        text =  input('New product:').replace(' ', '')
+        if len(text.split(';')) == 3:
             writeToFile(text, FILEPATH)
-        else: break
+            input('New data have been saved. Enter \'ENTER\' to continue.')
+            print('---------------------------------------')
+        elif text == 'q': 
+            print('---------------------------------------')
+            break
+    
     
 def searchProducts(criteria, value):
     productsList = readFromFile(FILEPATH)
+    correctProducts = []
     print('Search Result(s):')
     for productDict in productsList:
         if productDict[criteria] == value:
-            print(productDict)
+            correctProducts.append(productDict)
+    if correctProducts != []:
+        for productDict in correctProducts:
+            printProduct(productDict)
+    else: print('No Product has been found!\n')
+    input('Enter \'ENTER\' to continue.')
+    print('---------------------------------------')
+        
+def showAllProducts(filePath):
+    productsList = readFromFile(filePath)
+    if productsList!=[]:
+        for productDict in productsList:
+            printProduct(productDict)
+    else: print('No product has been found!\n')     
+    input('Enter \'ENTER\' to continue.')  
+    print('---------------------------------------') 
+        
+def printProduct(productDict):
+    print('name: ' + productDict['name'])
+    print('unit price: ' + productDict['unit_price'])
+    print('amount: ' + productDict['amount'])
+    print('---------------------------------------')
 
 if __name__ == '__main__':
-    inputProductsFromConsole()
-    criteria = input('Enter the criteria to search:')
-    value = input('Enter the value:')
-    searchProducts(criteria, value)
-    
+    while True:
+        print('******Products Information System******\n\n1. Enter new product data.\n2. Search by criteria in products database \n3. Show all products infomation.\n4. Quit.\n')
+        choice = input('Please select:')
+        print('---------------------------------------')
+        if choice == '1':
+            inputProductsFromConsole()
+        elif choice == '2':
+            criteria = input('Enter the criteria to search(name/unit_price/amount):')
+            value = input('Enter the value:')
+            searchProducts(criteria, value)
+        elif choice == '3':
+            showAllProducts(FILEPATH)
+        elif choice == '4':
+            print('Bye bye!')
+            break
+        else:
+            print('Error input! Please enter a number from 1 to 4!\n')
+            print('---------------------------------------')
+        
     
     
     
